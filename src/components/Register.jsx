75516dab2a6beb96ec5b938f47e39fe699577de8
radu <radustanciu841@yahoo.ro>
2021-07-29 22:56:26 +0300

    Add register form for a user account

---
+++ src/components/Register.jsx
@@ -0,0 +1,87 @@
+import React, { Component } from "react";
+import FormInput from "./FormInput";
+import ButtonSubmit from "./ButtonSubmit";
+
+export class Register extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    };
+  }
+  handleChangeName(event) {
+    const newName = event.target.value;
+    this.setState({ name: newName });
+  }
+  handleChangeEmail(event) {
+    const newEmail = event.target.value;
+    this.setState({ email: newEmail });
+  }
+  handleChangePassword(event) {
+    const newPassword = event.target.value;
+    this.setState({ password: newPassword });
+  }
+  handleChangeConfirmPassword(event) {
+    const newConfirmPassword = event.target.value;
+    this.setState({ confirmPassword: newConfirmPassword });
+  }
+
+  handleSubmitForm(event) {
+    event.preventDefault();
+    this.setState({
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    });
+  }
+  render() {
+    return (
+      <div className="sign-in">
+        <div className="sign-in-details">
+          <h4>I don't have an account</h4>
+          <span>SignUp with your email and password</span>
+        </div>
+        <form onSubmit={(event) => this.handleSubmitForm(event)}>
+          <FormInput
+            type="text"
+            name="name"
+            value={this.state.name}
+            label="name"
+            onChange={(name) => this.handleChangeName(name)}
+          />
+          <FormInput
+            type="email"
+            name="email"
+            value={this.state.email}
+            label="email"
+            onChange={(event) => this.handleChangeEmail(event)}
+          />
+          <FormInput
+            type="password"
+            name="password"
+            value={this.state.password}
+            label="password"
+            onChange={(event) => this.handleChangePassword(event)}
+          />
+          <FormInput
+            type="password"
+            name="confirmPassword"
+            value={this.state.confirmPassword}
+            label="confirmPassword"
+            onChange={(event) => this.handleChangeConfirmPassword(event)}
+          />
+          <div className='button-submit'>
+          <ButtonSubmit type='submit'>SignUp</ButtonSubmit>
+
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Register;
--- src/pages/SignInAndSignUp.jsx
+++ src/pages/SignInAndSignUp.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../pages/SignInAndSignUp.scss';
 import SignIn from '../components/SignIn';
+import Register from '../components/Register';
 
 function SignInAndSignUp() {
     return (
         <div className='sign-in-and-sign-up'>
             <SignIn />
+            <Register />
         </div>
     )
 }
