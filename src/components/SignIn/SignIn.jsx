8860002ea23f1ab94bbe8d0718280cec0a7d4126
radu <radustanciu841@yahoo.ro>
2021-08-15 19:54:59 +0300

    Convert SignIn with useState hook

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,37 +1,25 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "../SignIn/SignIn.scss";
 import FormInput from "../FormInput/FormInput";
 import ButtonSubmit from "../ButtonSubmit/ButtonSubmit";
 // import functia de autentificare cu Google
 import { signInWithGoogle } from "../../apis/firebase";
-import { withRouter } from "react-router-dom";
-export class SignIn extends Component {
-  constructor() {
-    super();
-    this.state = {
-      // initial campurile pentru email si parola sunt goale, necompletate in cadrul formularului
-      email: "",
-      password: "",
-    };
-  }
+import { useHistory } from "react-router";
+function SignIn() {
+  let history = useHistory();
+  const [userCredentials, setUserCredentials] = useState({
+    email: "",
+    password: "",
+  });
+
   // metoda de logare cu Google
   // prin history.push se redirectioneaza user-ul catre pagina de Home
-  handleSignInWithGoogle() {
+  function SignInWithGoogle() {
     signInWithGoogle();
-    this.props.history.push("/");
+    history.push("/");
   }
-  // metoda pentru a face update campupui de email
-  handleChangeEmail(event) {
-    const newEmail = event.target.value;
-    this.setState({ email: newEmail });
-  }
-  // metoda pentu a face update campului de password
-  handleChangePassword(event) {
-    const newPassword = event.target.value;
-    this.setState({ password: newPassword });
-  }
-  // metoda pentru submiterea formularului
-  handleSubmit(event) {
+
+  function handleSubmit(event) {
     //   prevenirea comportameentului default la formularului
     event.preventDefault();
     // resetarea formularului dupa completare
@@ -40,38 +28,37 @@ export class SignIn extends Component {
       password: "",
     });
   }
-  render() {
-    // in cadrul formularului dorim sa stilizam fiecare input, motiv pentru care vom crea o noua componenta simpla de tip FormInput
-    return (
+  return (
+    <div>
       <div className="sign-in">
         <div className="sign-in-details">
           <h4>I have already personal account</h4>
           <span>SignIn with your email and password</span>
         </div>
-        <form onSubmit={(event) => this.handleSubmit(event)}>
+        <form onSubmit={(event) => handleSubmit(event)}>
           {/* actuaziez inputului cu, componenta FormInput care are style-ul pregatit */}
           <FormInput
             type="email"
             name="email"
-            value={this.state.email}
+            value={userCredentials}
             label="email"
-            handleChange={(event) => this.handleChangeEmail(event)}
+            handleChange={(event) => setUserCredentials(event.target.value)}
             require
           />
           {/* actuaziez inputului cu, componenta FormInput care are style-ul pregatit */}
           <FormInput
             type="password"
             name="password"
-            value={this.state.password}
+            value={userCredentials}
             label="password"
-            handleChange={(event) => this.handleChangePassword(event)}
+            handleChange={(event) => setUserCredentials(event.target.value)}
             require
           />
           <div className="button-submit">
             <ButtonSubmit type="submit">Sign In</ButtonSubmit>
             {/* atasez la butonul de submit eventul onClick cu functia importata */}
             <ButtonSubmit
-              onClick={() => this.handleSignInWithGoogle()}
+              onClick={() => SignInWithGoogle()}
               className="sign-in-with-google"
             >
               Sign In with Google
@@ -79,8 +66,8 @@ export class SignIn extends Component {
           </div>
         </form>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-export default withRouter(SignIn);
+export default SignIn;
